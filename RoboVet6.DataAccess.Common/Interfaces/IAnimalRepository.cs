4e924cb49f8a045f18f46fdda8f43d929c4016e6
jack.cop07 <jack.copeland7@gmail.com>
2020-12-16 20:46:34 +0000

    Fixing missing files - working build

---
+++ RoboVet6.DataAccess.Common/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RoboVet6.Data.Models;
+
+namespace RoboVet6.DataAccess.Common.Interfaces
+{
+    public interface IAnimalRepository
+    {
+        Task<List<Animal>> GetAllAnimals();
+        Task<Animal> GetAnimalByAnimalId(int animalId);
+        Task<List<Animal>> GetAnimalsByClientId(int clientId);
+        Task InsertAnimal(Animal animal);
+        Task UpdateAnimal(Animal animal);
+        Task<bool> AnimalExists(int animalId);
+    }
+}
