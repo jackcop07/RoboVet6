d220b8413a14dadb21d3231a59524d8afacddf0c
jack.cop07 <jack.copeland7@gmail.com>
2021-02-27 22:25:07 +0000

    Updated UI, shows clients with animals - talking fine with API

---
+++ RoboVet6.Blazor.UI/Pages/ClientDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboVet6.Blazor.UI.Pages
+{
+    public class ClientDetail
+    {
+    }
+}
