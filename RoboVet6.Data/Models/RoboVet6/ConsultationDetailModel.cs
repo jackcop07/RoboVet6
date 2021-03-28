13faeeb99cf195f1f9204498d89d8fd443d0550c
jack.cop07 <jack.copeland7@gmail.com>
2021-03-28 16:03:34 +0100

    Added client search. Added new models for consult, detail and products.

---
+++ RoboVet6.Data/Models/RoboVet6/ConsultationDetailModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace RoboVet6.Data.Models.RoboVet6
+{
+    public class ConsultationDetailModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("ConsultationModel")]
+        public int ConsultationId { get; set; }
+        public ConsultationModel Consultation { get; set; }
+
+        [Required]
+        [ForeignKey("ProductModel")]
+        public int ProductId { get; set; }
+        public ProductModel Product { get; set; }
+
+        [Required]
+        public long Quantity { get; set; }
+
+        public decimal Discount { get; set; } = 0;
+
+        [Required]
+        public decimal PriceIncVat { get; set; }
+
+        [Required]
+        public decimal PriceExcVat { get; set; }
+
+    }
+}
---
+++ RoboVet6.Data/Models/RoboVet6/ConsultationModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RoboVet6.Data.Models.RoboVet6
+{
+    public class ConsultationModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime ConsultationDate { get; set; }
+
+        [Required]
+        public string Employee { get; set; }
+
+        [Required]
+        [ForeignKey("AnimalModel")]
+        public int AnimalId { get; set; }
+
+        public IEnumerable<ConsultationDetailModel> ConsultationDetails { get; set; }
+
+        [Required] 
+        public decimal SaleTotalIncVat { get; set; } = 0;
+
+        [Required] 
+        public decimal SaleTotalExcVat { get; set; } = 0;
+    }
+}
